a277973d79cd4465aaa83313782e881083f34c03
Toni <toniojeda2015@gmail.com>
2024-11-15 18:57:50 -0600

    added sliding animation to image and about text

--- actions.js
+++ actions.js
@@ -2,19 +2,41 @@
 $(loadPage);
 
 function loadPage(){
-    let $resumeOption = $("#resume");
-    $resumeOption.click(selectedHandler);
-    console.log("here")
-    
+    loadEventListeners();
 }
 
-function selectedHandler(){
-    console.log("here")
-    let text = $(this).text()
-    
-    if(text == "Resume"){
-        $("object").attr("data","ResumeFinal.pdf#toolbar=0");
-        console.log("here")
+function loadEventListeners(){
+    $("#homeImg").mouseover(function(){
+        
+        $("html").css("--after-width","100%");
 
-    }
+    })
+    $("#homeImg").mouseout(function(){
+        console.log("here");
+        console.log($(".nameContent::after"));
+        $("html").css("--after-width","0%");
+
+    })
+    $("#homeImg").click(function(){
+        $(".nameContent").hide("slow").queue(function(next){
+            $(".nameContent").css({
+                "margin-left": "0",
+                "margin-right": "15vw",
+                right: 0,
+                padding: "1vw 13vw 5vw 15vw"
+            })
+        $(".nameContent").html("");
+        $("#homeImg").animate({
+            right: "-=70vw"
+        },300,function(){
+                console.log("here")
+                $(".nameContent").show("slow");
+            next()
+           })});
+        ;
+        })
+        
+
+
 }
+    
