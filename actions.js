ddc323112b7a63c90fe899709e436e0d92e15410
Toni <toniojeda2015@gmail.com>
2024-11-16 04:01:10 -0600

    added text to be shown on img click

--- actions.js
+++ actions.js
@@ -24,13 +24,16 @@ function loadEventListeners(){
 function imgClick(){
     let $topTextBox = $(".nameContent")
     let $topImg = $("#homeImg")
-    let $unclickedText = $("#clicked")
+    let $unclickedText = $("#unclicked")
+    let $clickedText = $("#clicked")
         //sliding effect on image and text content
     if(eventRecords.imgClicked){
         eventRecords.imgClicked = false;
         console.log("1left "+$topTextBox.css("left"))
         console.log("1right "+$topTextBox.css("right"))
         $topTextBox.hide("slow").queue(function(next){
+            $unclickedText.removeClass("hidden")
+            $clickedText.addClass("hidden")
             $topTextBox.css({
                 "margin-left": "15vw",
                 "margin-right": "0",
@@ -52,11 +55,13 @@ function imgClick(){
         console.log("left "+$topTextBox.css("left"))
         console.log("right "+$topTextBox.css("right"))
         $topTextBox.hide("slow").queue(function(next){
-        $topTextBox.css({
-            "margin-left": "0",
-            "margin-right": "15vw",
-            direction: "rtl",
-            right: "0px"
+            $clickedText.removeClass("hidden")
+            $unclickedText.addClass("hidden")
+            $topTextBox.css({
+                "margin-left": "0",
+                "margin-right": "15vw",
+                direction: "rtl",
+                right: "0px"
 
             
             
