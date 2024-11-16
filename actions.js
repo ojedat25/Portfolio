a36e221faf6c2f5f305bb756e7af17d24111553a
Toni <toniojeda2015@gmail.com>
2024-11-16 03:44:44 -0600

    Fixed bugs

--- actions.js
+++ actions.js
@@ -1,5 +1,8 @@
 //Waits until DOM is fully loaded
 $(loadPage);
+var eventRecords = [
+    {imgClicked: false}
+]
 
 function loadPage(){
     loadEventListeners();
@@ -12,29 +15,63 @@ function loadEventListeners(){
 
     })
     $("#homeImg").mouseout(function(){
-        console.log("here");
-        console.log($(".nameContent::after"));
         $("html").css("--after-width","0%");
 
     })
-    $("#homeImg").click(function(){
-        $(".nameContent").hide("slow").queue(function(next){
-            $(".nameContent").css({
-                "margin-left": "0",
-                "margin-right": "15vw",
-                right: 0,
-                padding: "1vw 13vw 5vw 15vw"
+    $("#homeImg").click(imgClick);
+}
+
+function imgClick(){
+    let $topTextBox = $(".nameContent")
+    let $topImg = $("#homeImg")
+    let $unclickedText = $("#clicked")
+        //sliding effect on image and text content
+    if(eventRecords.imgClicked){
+        eventRecords.imgClicked = false;
+        console.log("1left "+$topTextBox.css("left"))
+        console.log("1right "+$topTextBox.css("right"))
+        $topTextBox.hide("slow").queue(function(next){
+            $topTextBox.css({
+                "margin-left": "15vw",
+                "margin-right": "0",
+                direction: "ltr",
+               right:"initial"
+                
+                
             })
-        $(".nameContent").html("");
-        $("#homeImg").animate({
+            $topImg.animate({
+                right:"+=70vw"
+            },300,function(){
+                $topTextBox.show("slow");
+                next();
+            })});
+
+        }
+    else{
+        eventRecords.imgClicked = true;
+        console.log("left "+$topTextBox.css("left"))
+        console.log("right "+$topTextBox.css("right"))
+        $topTextBox.hide("slow").queue(function(next){
+        $topTextBox.css({
+            "margin-left": "0",
+            "margin-right": "15vw",
+            direction: "rtl",
+            right: "0px"
+
+            
+            
+        })
+        $topImg.animate({
             right: "-=70vw"
         },300,function(){
                 console.log("here")
-                $(".nameContent").show("slow");
-            next()
-           })});
-        ;
-        })
+                $topTextBox.show("slow");
+            
+            })
+        next()
+        });
+    
+    }
         
 
 
