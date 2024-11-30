28fc176a12e879877dda03260a2e8252e122bdee
Toni <toniojeda2015@gmail.com>
2024-11-30 03:40:48 -0600

    Added section animations when scrolling (home section does not go back to normal after scrolling down, fix)

--- actions.js
+++ actions.js
@@ -5,92 +5,104 @@ var eventRecords = [{ imgClicked: false }];
 
 function loadPage() {
   loadEventListeners();
-  window.addEventListener("scroll", ScrollEvent);
+  $(window).scroll(ScrollEvent);
 }
 
 function ScrollEvent() {
-  const nodes = document.querySelectorAll(".progressBar")
-  const scroll = window.scrollY/5;
-  const sectionTop = document.querySelector("#Skills").getBoundingClientRect().top
-  const sectionBottom = document.querySelector("#Skills").getBoundingClientRect().bottom
-  console.log("SectionTop: "+ sectionTop);
-  console.log("SectionBottom: "+sectionBottom);
-  console.log("ScrollY: "+scroll);
+  const mainNode = $('body,html').children(); //list of direct children
+  const nodes = document.querySelectorAll('.progressBar');
+  const screenTop = $(window).scrollTop(); //how far we have scrolled from the top
+  const screenBottom = screenTop + $(window).innerHeight();
+  const nodeTop = $('#Skills').offset().top;
+  const nodeBottom = $('#Skills').outerHeight() + nodeTop;
   nodes.forEach((node) => {
-    const width = Math.min(parseInt(node.textContent.substring(0,2)),scroll);
-    if(sectionBottom != (sectionBottom/2)){
-    node.style.width = (width - 22) + "%";
-    
-    }
-    else{
-      node.style.width = 0 + "%";
+    const width = Math.min(parseInt(node.textContent.substring(0, 2)), screenTop);
+    if (screenBottom > nodeTop && screenTop < nodeBottom) {
+      node.style.width = width - 22 + '%';
+      console.log('node width: ' + node.style.width);
+    } else {
+      node.style.width = 0 + '%';
+      console.log('here');
     }
   });
+  // console.log("SectionTop: "+ sectionTop);
+  // console.log("SectionBottom: "+sectionBottom);
+  // console.log("ScrollY: "+scroll);
+  for (let i = 3; i < mainNode.length - 1; i++) {
+    const sectionTop = $(mainNode[i]).offset().top;
+    const sectionBottom = sectionTop + $(mainNode[i]).outerHeight(); //getting the sections bottom position
 
-
+    if (screenBottom > sectionTop && screenTop < sectionBottom) {
+      if ($(mainNode[i]).attr('id') == 'Home') {
+        $(mainNode[3]).children('.animatedDiv').removeClass('divAnimation');
+      } else {
+        $(mainNode[5]).children('.animatedDiv').removeClass('divAnimation');
+      }
+    } else if ($(mainNode[i]).attr('id') != 'Skills') {
+      $(mainNode[i]).children('.animatedDiv').addClass('divAnimation');
+    }
+  }
 }
 
-
 function loadEventListeners() {
-  $("#homeImg").mouseover(function () {
+  $('#homeImg').mouseover(function () {
     //Increases the width of the nameContent div pseudo after effect ::after
-    $("html").css("--after-width", "100%");
+    $('html').css('--after-width', '100%');
   });
-  $("#homeImg").mouseout(function () {
+  $('#homeImg').mouseout(function () {
     //Decreases the width of the nameContent div pseudo after effect ::after
-    $("html").css("--after-width", "0%");
+    $('html').css('--after-width', '0%');
   });
-  $("#homeImg").click(imgClick);
-  $("#Form").on("submit", formSubmit);
+  $('#homeImg').click(imgClick);
+  $('#Form').on('submit', formSubmit);
 }
 
 function imgClick() {
-  let $topTextBox = $(".nameContent");
-  let $topImg = $("#homeImg");
-  let $unclickedText = $("#unclicked");
-  let $clickedText = $("#clicked");
+  let $topTextBox = $('.nameContent');
+  let $topImg = $('#homeImg');
+  let $unclickedText = $('#unclicked');
+  let $clickedText = $('#clicked');
   //sliding effect on image and text content
   if (eventRecords.imgClicked) {
     eventRecords.imgClicked = false;
-    $topTextBox.hide("slow").queue(function (next) {
-      $unclickedText.removeClass("hidden");
-      $clickedText.addClass("hidden");
+    $topTextBox.hide('slow').queue(function (next) {
+      $unclickedText.removeClass('hidden');
+      $clickedText.addClass('hidden');
       $topTextBox.css({
-        "margin-left": "15vw",
-        "margin-right": "0",
-        direction: "ltr",
-        right: "initial", //resets value
+        'margin-left': '15vw',
+        'margin-right': '0',
+        direction: 'ltr',
+        right: 'initial', //resets value
       });
       $topImg.animate(
         {
-          right: "+=70vw",
+          right: '+=70vw',
         },
         300,
         function () {
-          $topTextBox.show("slow");
+          $topTextBox.show('slow');
           next();
         }
       );
     });
   } else {
     eventRecords.imgClicked = true;
-    $topTextBox.hide("slow").queue(function (next) {
-      $clickedText.removeClass("hidden");
-      $unclickedText.addClass("hidden");
+    $topTextBox.hide('slow').queue(function (next) {
+      $clickedText.removeClass('hidden');
+      $unclickedText.addClass('hidden');
       $topTextBox.css({
-        "margin-left": "0",
-        "margin-right": "15vw",
-        direction: "rtl", //changes direction the pseduo element ::after of nameContent's direction to right to left
-        right: "0px",
+        'margin-left': '0',
+        'margin-right': '15vw',
+        direction: 'rtl', //changes direction the pseduo element ::after of nameContent's direction to right to left
+        right: '0px',
       });
       $topImg.animate(
         {
-          right: "-=70vw",
+          right: '-=70vw',
         },
         300,
         function () {
-          console.log("here");
-          $topTextBox.show("slow");
+          $topTextBox.show('slow');
         }
       );
       next();
@@ -98,33 +110,33 @@ function imgClick() {
   }
 }
 function formSubmit(event) {
-  console.log("Making Submission");
-  let $result = $("#result");
-  let form = document.getElementById("Form");
-  $result.removeClass("hidden");
+  console.log('Making Submission');
+  let $result = $('#result');
+  let form = document.getElementById('Form');
+  $result.removeClass('hidden');
   event.preventDefault();
   const formData = new FormData(form);
   const object = Object.fromEntries(formData);
   const json = JSON.stringify(object);
-  $result.text("Please wait...");
+  $result.text('Please wait...');
   $.ajax({
-    url: "https://api.web3forms.com/submit",
-    method: "POST",
+    url: 'https://api.web3forms.com/submit',
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
-    datatype: "json",
+    datatype: 'json',
     data: json,
     success: function (res) {
       console.log(res.message);
       $result.text(res.message);
     },
   }).fail(function (err) {
-    $result.text("Something Went Wrong");
+    $result.text('Something Went Wrong');
   });
   form.reset();
   setTimeout(function () {
-    $result.addClass("hidden");
+    $result.addClass('hidden');
   }, 3000);
 }
